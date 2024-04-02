2d815e79f15468136c911f6a5f22dbdeb995e98b
nikunj.basnet <leavem8@gmail.com>
2024-04-02 16:20:09 +0545

    new

--- datatypes/datatypes.js
+++ datatypes/datatypes.js
@@ -2,12 +2,16 @@ let message = "great";
 message = 1234; //current value is printed//
 console.log(message);
 
-console.log(NaN + 1);// always nan undefined, it prints nan 
+console.log(NaN + 1); // always nan undefined, it prints nan
 
 //strings//
 let string = "hey";
-let string2= " there ";
-let join = `${string}  ${string2}`;//back tick to join the variables value 
- 
+let string2 = " there ";
+let join = `${string}  ${string2}`; //back tick to join the variables value
+
 // console.log(`so the result is ${string}`);
 console.log(join);
+
+let say = "hello",
+  output = say;
+console.log(output);
